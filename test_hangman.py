35d7c875d1d2ef36629e309d4275b2dd24ccb4f9
jishnu <givemeakiss111@gmail.com>
2023-11-02 17:10:13 +0530

    Created a function to check puctuation

--- test_hangman.py
+++ test_hangman.py
@@ -11,3 +11,13 @@ def test_random_word_lowercase():
         assert hangman.get_random_word(fname) == "apple"
 
     os.unlink(fname)
+
+def test_random_word_no_punctuation():
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["pineapple\n", "mango's\n", '"beryl"'])
+
+    for _ in range(100):
+        assert hangman.get_random_word(fname) == "pineapple"
+
+    os.unlink(fname)
